da4511754aaa8b0d4b368f9fe57c9df9ce600032
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-09 22:48:03 +0300

    create file messages

---
+++ 02.Naming_Identifiers/Naming Identifiers Homework/Application2/Models/Messages.cs
@@ -0,0 +1,9 @@
+﻿namespace Mines.Models
+{
+    public static class Messages
+    {
+        public const string WelcomeMessage = "Come to play “MINES”. Try your luck to find the flight without mines.";
+        public const string HelpMessage = "Command 'top' show rating, 'restart' starts a new game, 'exit' out of the game!";
+        public const string EmptyRaiting = "Rating is empty!\n";
+    }
+}
