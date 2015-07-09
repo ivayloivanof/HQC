4ca8e67fc59881e23676b12542061521cf9277d9
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-09 22:13:36 +0300

    creating and completion of the class Point

---
+++ 02.Naming_Identifiers/Naming Identifiers Homework/Application2/Models/Point.cs
@@ -0,0 +1,51 @@
+﻿namespace Mines.Models
+{
+    using System;
+
+    class Point
+    {
+        private string name;
+        private int points;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name", "This name is empty!");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Points
+        {
+            get
+            {
+                return this.points;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Points", "This points cannot be negative!");
+                }
+                this.points = value;
+            }
+        }
+
+        public Point(string name, int points)
+        {
+            this.Name = name;
+            this.Points = points;
+        }
+    }
+}
