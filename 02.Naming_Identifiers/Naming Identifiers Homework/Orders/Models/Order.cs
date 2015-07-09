4eb6bee06f732fd3546e815ea94604583544c413
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-09 13:45:11 +0300

    change all names of the property

--- 02.Naming_Identifiers/Naming Identifiers Homework/Orders/Models/Order.cs
+++ 02.Naming_Identifiers/Naming Identifiers Homework/Orders/Models/Order.cs
@@ -4,9 +4,9 @@
     {
         public int ID { get; set; }
 
-        public int product_id { get; set; }
+        public int ProductID { get; set; }
 
-        public int quant { get; set; }
+        public int Quant { get; set; }
 
         public decimal Discount { get; set; }
     }
