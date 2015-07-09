e1ded790ae560d7afad695cf866136540b0fce68
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-09 16:17:42 +0300

    create class rating and add static method ViewRating

---
+++ 02.Naming_Identifiers/Naming Identifiers Homework/Application2/Models/Rating.cs
@@ -0,0 +1,26 @@
+﻿namespace Mines.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class Rating
+    {
+        private static void ViewRating(List<Minesweeper.zaKlasiraneto> to4kii)
+        {
+            Console.WriteLine("\nTo4KI:");
+            if (to4kii.Count > 0)
+            {
+                for (int i = 0; i < to4kii.Count; i++)
+                {
+                    Console.WriteLine("{0}. {1} --> {2} kutii", i + 1, to4kii[i].igra4, to4kii[i].kolko);
+                }
+
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("prazna klasaciq!\n");
+            }
+        }
+    }
+}
