5536250771c1c8afde489aa7f110cf373be47631
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-09 13:45:27 +0300

    change all names of the property in source code

--- 02.Naming_Identifiers/Naming Identifiers Homework/Orders/Orders.cs
+++ 02.Naming_Identifiers/Naming Identifiers Homework/Orders/Orders.cs
@@ -17,17 +17,17 @@ namespace Orders
 
             // Names of the 5 most expensive products
             var first = allProducts
-                .OrderByDescending(p => p.unit_price)
+                .OrderByDescending(p => p.UnitPrice)
                 .Take(5)
-                .Select(p => p.nome);
+                .Select(p => p.Name);
             Console.WriteLine(string.Join(Environment.NewLine, first));
 
             Console.WriteLine(new string('-', 10));
 
             // Number of products in each Category
             var second = allProducts
-                .GroupBy(p => p.catId)
-                .Select(grp => new { Category = allCategories.First(c => c.Id == grp.Key).NAME, Count = grp.Count() })
+                .GroupBy(p => p.CatID)
+                .Select(grp => new { Category = allCategories.First(c => c.ID == grp.Key).Name, Count = grp.Count() })
                 .ToList();
             foreach (var item in second)
             {
@@ -38,8 +38,8 @@ namespace Orders
 
             // The 5 top products (by Order quantity)
             var third = allOrders
-                .GroupBy(o => o.product_id)
-                .Select(grp => new { Product = allProducts.First(p => p.id == grp.Key).nome, Quantities = grp.Sum(grpgrp => grpgrp.quant) })
+                .GroupBy(o => o.ProductID)
+                .Select(grp => new { Product = allProducts.First(p => p.ID == grp.Key).Name, Quantities = grp.Sum(grpgrp => grpgrp.Quant) })
                 .OrderByDescending(q => q.Quantities)
                 .Take(5);
             foreach (var item in third)
@@ -51,10 +51,10 @@ namespace Orders
 
             // The most profitable Category
             var category = allOrders
-                .GroupBy(o => o.product_id)
-                .Select(g => new { catId = allProducts.First(p => p.id == g.Key).catId, price = allProducts.First(p => p.id == g.Key).unit_price, quantity = g.Sum(p => p.quant) })
+                .GroupBy(o => o.ProductID)
+                .Select(g => new { catId = allProducts.First(p => p.ID == g.Key).CatID, price = allProducts.First(p => p.ID == g.Key).UnitPrice, quantity = g.Sum(p => p.Quant) })
                 .GroupBy(gg => gg.catId)
-                .Select(grp => new { category_name = allCategories.First(c => c.Id == grp.Key).NAME, total_quantity = grp.Sum(g => g.quantity * g.price) })
+                .Select(grp => new { category_name = allCategories.First(c => c.ID == grp.Key).Name, total_quantity = grp.Sum(g => g.quantity * g.price) })
                 .OrderByDescending(g => g.total_quantity)
                 .First();
             Console.WriteLine("{0}: {1}", category.category_name, category.total_quantity);
