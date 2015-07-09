9c5c4041b555e8087469f18dfb8868baf48404d1
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-09 13:44:27 +0300

    change all names of the property

--- 02.Naming_Identifiers/Naming Identifiers Homework/Orders/Models/Category.cs
+++ 02.Naming_Identifiers/Naming Identifiers Homework/Orders/Models/Category.cs
@@ -2,9 +2,9 @@
 {
     public class Category
     {
-        public int Id { get; set; }
+        public int ID { get; set; }
         
-        public string NAME { get; set; }
+        public string Name { get; set; }
 
         public string Description { get; set; }
     }
