0a313020ef9bd54127d78ccf8e24e9d7e7c6c4f5
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-09 23:28:07 +0300

    add more messages

--- 02.Naming_Identifiers/Naming Identifiers Homework/Application2/Models/Messages.cs
+++ 02.Naming_Identifiers/Naming Identifiers Homework/Application2/Models/Messages.cs
@@ -5,5 +5,9 @@
         public const string WelcomeMessage = "Come to play “MINES”. Try your luck to find the flight without mines.";
         public const string HelpMessage = "Command 'top' show rating, 'restart' starts a new game, 'exit' out of the game!";
         public const string EmptyRaiting = "Rating is empty!\n";
+        public const string GiveRowAndColumn = "Give row and column : ";
+        public const string ErrorCommand = "\nERROR! Invalid command!\n";
+        public const string MadeInBulgaria = "Made in Bulgaria!";
+        public const string GoodBye = "GoodBye!";
     }
 }
