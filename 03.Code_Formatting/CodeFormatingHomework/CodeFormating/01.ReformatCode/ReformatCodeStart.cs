5abf0c8020fdb8e6fb734fcc83a5e39e10f9045e
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-11 00:33:22 +0300

    reformat code

--- 03.Code_Formatting/CodeFormatingHomework/CodeFormating/01.ReformatCode/ReformatCodeStart.cs
+++ 03.Code_Formatting/CodeFormatingHomework/CodeFormating/01.ReformatCode/ReformatCodeStart.cs
@@ -5,156 +5,106 @@
 
     public class ReformatCodeStart
     {
-        private readonly StringBuilder output = new StringBuilder();
+        private static StringBuilder output = new StringBuilder();
+        private static EventHolder events = new EventHolder();
 
-        class EventHolder
+        public static void Main()
         {
-            MultiDictionary<string, Event>
-                byTitle = new MultiDictionary<string,
-                Event>(true);
-            OrderedBag<Event>
-                byDate = new OrderedBag<
-                Event>();
-
-            public void AddEvent(DateTime date,
-                                    string title,
-                                    string location)
+            while (ExecuteNextCommand())
             {
-                Event newEvent = new Event(date,
-                                            title,
-                                            location);
-                byTitle.Add(
-                            title.ToLower(),
-                            newEvent
-                            );
-                byDate.Add(newEvent); Messages.EventAdded();
             }
 
-            public void DeleteEvents(string titleToDelete)
-            {
-                string title = titleToDelete
-                        .ToLower();
-                int removed = 0;
-                foreach (var eventToRemove in byTitle[title])
-                {
-                    removed++;
-                    byDate.Remove(eventToRemove);
-                }
-                byTitle.Remove(title);
-                Messages.EventDeleted(removed);
-            }
-            public void ListEvents(DateTime date, int count)
-            {
-                OrderedBag<Event>.View
-                        eventsToShow = byDate.RangeFrom(new Event(
-                        date, "", ""),
-                        true);
-                int showed = 0;
-                foreach (var eventToShow in eventsToShow)
-                {
-                    if (showed == count) break;
-                    Messages.PrintEvent(eventToShow);
-
-                    showed++;
-                }
-                if (showed == 0) Messages.NoEventsFound();
-            }
-        }
-
-        static EventHolder events = new EventHolder();
-
-        static void Main(string[] args)
-        {
-            while (ExecuteNextCommand()) { }
             Console.WriteLine(output);
         }
 
         private static bool ExecuteNextCommand()
         {
             string command = Console.ReadLine();
-            if (command[0] == 'A') { AddEvent(command); return true; }
-            if (command[0] == 'D') { DeleteEvents(command); return true; }
 
+            if (command[0] == 'A')
+            {
+                AddEvent(command);
+                {
+                    return true;
+                }
+            }
 
-            if (command[0] == 'L') { ListEvents(command); return true; }
-            if (command[0] == 'E') { return false; }
+            if (command[0] == 'D')
+            {
+                DeleteEvents(command);
+                {
+                    return true;
+                }
+            }
+
+            if (command[0] == 'L')
+            {
+                ListEvents(command);
+                {
+                    return true;
+                }
+            }
+
+            if (command[0] == 'E')
+            {
+                return false;
+            }
+
             return false;
         }
 
         private static void ListEvents(string command)
         {
-            int pipeIndex = command.IndexOf('|')
-            ;
-            DateTime date = GetDate(
-            command,
-            "ListEvents");
-            string countString = command.Substring(
-                pipeIndex + 1
-            );
-
-
+            int pipeIndex = command.IndexOf('|');
+            DateTime date = GetDate(command, "ListEvents");
+            string countString = command.Substring(pipeIndex + 1);
             int count = int.Parse(countString);
-            events.ListEvents(date,
-            count);
+            events.ListEvents(date, count);
         }
 
-        private static void
-
-
-            DeleteEvents(string command)
+        private static void DeleteEvents(string command)
         {
-            string title = command.Substring
-            (
-                "DeleteEvents".Length + 1
-            );
+            string title = command.Substring("DeleteEvents".Length + 1);
             events.DeleteEvents(title);
         }
 
         private static void AddEvent(string command)
         {
-            DateTime date; string title; string location;
-            GetParameters(command, "AddEvent",
-                    out date, out title, out location);
+            DateTime date; 
+            string title; 
+            string location;
+            GetParameters(command, "AddEvent", out date, out title, out location);
 
-            events.AddEvent
-                    (date, title, location);
+            events.AddEvent(date, title, location);
         }
-        private static void GetParameters(string commandForExecution, string commandType, out DateTime dateAndTime, out string eventTitle, out string eventLocation)
+
+        private static void GetParameters(
+            string commandForExecution,
+            string commandType,
+            out DateTime dateAndTime, 
+            out string eventTitle, 
+            out string eventLocation)
         {
             dateAndTime = GetDate(commandForExecution, commandType);
             int firstPipeIndex = commandForExecution.IndexOf('|');
 
-
             int lastPipeIndex = commandForExecution.LastIndexOf('|');
             if (firstPipeIndex == lastPipeIndex)
             {
-                eventTitle =
-                    commandForExecution.Substring(firstPipeIndex
-                    + 1).Trim();
-                eventLocation = "";
+                eventTitle = commandForExecution.Substring(firstPipeIndex + 1).Trim();
+                eventLocation = string.Empty;
             }
             else
             {
-                eventTitle = commandForExecution.Substring(
-                                                        firstPipeIndex + 1,
-                                                        lastPipeIndex - firstPipeIndex - 1)
-                                                    .Trim();
+                eventTitle = commandForExecution.Substring(firstPipeIndex + 1, lastPipeIndex - firstPipeIndex - 1).Trim();
                 eventLocation = commandForExecution.Substring(lastPipeIndex + 1).Trim();
             }
         }
-        private
-        static
-        DateTime
-        GetDate(
 
-        string command,
-        string commandType
-        )
+        private static DateTime GetDate(string command, string commandType)
         {
-            DateTime date = DateTime.Parse(command
-                .Substring(commandType.Length +
+            DateTime date = DateTime.Parse(command.Substring(commandType.Length + 1, 20));
 
-                1, 20));
             return date;
         }
     }
