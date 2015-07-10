3eaa6ae66ffde40323665636cb738ac7d4f12025
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-10 23:35:21 +0300

    refactoring all

--- 02.Naming_Identifiers/Naming Identifiers Homework/Application2/Minesweeper.cs
+++ 02.Naming_Identifiers/Naming Identifiers Homework/Application2/Minesweeper.cs
@@ -13,93 +13,51 @@
             List<Point> champions = new List<Point>(6);
             Field fields = new Field();
 
-
             char[,] field = fields.CreatePlayingField();
             char[,] bombs = Field.LayingBombs();
 
-
-            bool flag = true;
-            bool flag2 = false;
+            bool startGame = true;
+            bool startNewGame = false;
+            bool endGame = false;
 
             int counter = 0;
-            bool grum = false;
             int row = 0;
             int column = 0;
-            const int maks = 35;
+            const int Max = 35;
 
             do
             {
-                if (flag)
+                if (startGame)
                 {
                     Console.WriteLine(Messages.WelcomeMessage + " " + Messages.HelpMessage);
                     Field.PrintMatrix(field);
-                    flag = false;
+                    startGame = false;
                 }
 
-                Console.Write(Messages.GiveRowAndColumn);
-                var readLine = Console.ReadLine();
-                if (readLine != null)
+                if (startNewGame)
                 {
-                    command = readLine.Trim();
-                    if (command == "exit")
-                    {
-                        break;
-                    }
+                    Console.WriteLine(Messages.Bravo);
+                    Field.PrintMatrix(bombs);
+                    Console.WriteLine(Messages.GiveYourName);
+                    string name = Console.ReadLine();
+                    Point points = new Point(name, counter);
+                    champions.Add(points);
+                    Rating.ViewRating(champions);
+
+                    // create new field (board)
+                    field = fields.CreatePlayingField();
+                    bombs = Field.LayingBombs();
+                    counter = 0;
+                    startNewGame = false;
+                    startGame = true;
                 }
 
-                if (command.Length >= 3)
-                {
-                    if (int.TryParse(command[0].ToString(), out row) && int.TryParse(command[2].ToString(), out column)
-                        && row <= field.GetLength(0) && column <= field.GetLength(1))
-                    {
-                        command = "turn";
-                    }
-                }
-
-                switch (command)
-                {
-                    case "top":
-                        Rating.ViewRating(champions);
-                        break;
-                    case "restart":
-                        field = fields.CreatePlayingField();
-                        bombs = Field.LayingBombs();
-                        Field.PrintMatrix(field);
-                        grum = false;
-                        flag = false;
-                        break;
-                    case "turn":
-                        if (bombs[row, column] != '*')
-                        {
-                            if (bombs[row, column] == '-')
-                            {
-                                tisinahod(field, bombs, row, column);
-                                counter++;
-                            }
-
-                            if (maks == counter)
-                            {
-                                flag2 = true;
-                            }
-                            else
-                            {
-                                Field.PrintMatrix(field);
-                            }
-                        }
-                        else
-                        {
-                            grum = true;
-                        }
-
-                        break;
-                }
-
-                if (grum)
+                if (endGame)
                 {
                     Field.PrintMatrix(bombs);
-                    Console.Write("\nHrrrrrr! Umria gerojski s {0} to4ki. " + "Daj si niknejm: ", counter);
-                    string niknejm = Console.ReadLine();
-                    Point t = new Point(niknejm, counter);
+                    Console.Write(Messages.EndGame, counter);
+                    string name = Console.ReadLine();
+                    Point t = new Point(name, counter);
                     if (champions.Count < 5)
                     {
                         champions.Add(t);
@@ -124,24 +82,68 @@
                     field = fields.CreatePlayingField();
                     bombs = Field.LayingBombs();
                     counter = 0;
-                    grum = false;
-                    flag = true;
+                    endGame = false;
+                    startGame = true;
                 }
 
-                if (flag2)
+                Console.Write(Messages.GiveRowAndColumn);
+                var readLine = Console.ReadLine();
+                if (readLine != null)
                 {
-                    Console.WriteLine("\nBRAVOOOS! Otvri 35 kletki bez kapka kryv.");
-                    Field.PrintMatrix(bombs);
-                    Console.WriteLine("Daj si imeto, batka: ");
-                    string imeee = Console.ReadLine();
-                    Point points = new Point(imeee, counter);
-                    champions.Add(points);
-                    Rating.ViewRating(champions);
-                    field = fields.CreatePlayingField();
-                    bombs = Field.LayingBombs();
-                    counter = 0;
-                    flag2 = false;
-                    flag = true;
+                    command = readLine.Trim();
+                    if (command == "exit")
+                    {
+                        break;
+                    }
+                }
+
+                if (command.Length >= 3)
+                {
+                    if (int.TryParse(command[0].ToString(), out row) && 
+                        int.TryParse(command[2].ToString(), out column) && 
+                        row <= field.GetLength(0) && 
+                        column <= field.GetLength(1))
+                    {
+                        command = "turn";
+                    }
+                }
+
+                switch (command)
+                {
+                    case "top":
+                        Rating.ViewRating(champions);
+                        break;
+                    case "restart":
+                        field = fields.CreatePlayingField();
+                        bombs = Field.LayingBombs();
+                        Field.PrintMatrix(field);
+                        endGame = false;
+                        startGame = false;
+                        break;
+                    case "turn":
+                        if (bombs[row, column] != '*')
+                        {
+                            if (bombs[row, column] == '-')
+                            {
+                                Move(field, bombs, row, column);
+                                counter++;
+                            }
+
+                            if (Max == counter)
+                            {
+                                startNewGame = true;
+                            }
+                            else
+                            {
+                                Field.PrintMatrix(field);
+                            }
+                        }
+                        else
+                        {
+                            endGame = true;
+                        }
+
+                        break;
                 }
             }
             while (command != "exit");
@@ -149,105 +151,86 @@
             Console.WriteLine(Messages.GoodBye);
         }
         
-
-        private static void tisinahod(char[,] POLE, char[,] BOMBI, int RED, int KOLONA)
+        private static void Move(char[,] board, char[,] bombs, int row, int col)
         {
-            char kolkoBombi = kolko(BOMBI, RED, KOLONA);
-            BOMBI[RED, KOLONA] = kolkoBombi;
-            POLE[RED, KOLONA] = kolkoBombi;
+            char kolkoBombi = kolko(bombs, row, col);
+            bombs[row, col] = kolkoBombi;
+            board[row, col] = kolkoBombi;
         }
-
         
-
-        private static void smetki(char[,] pole)
+        private static char kolko(char[,] board, int row, int col)
         {
-            int kol = pole.GetLength(0);
-            int red = pole.GetLength(1);
+            int piece = 0;
+            int reds = board.GetLength(0);
+            int kols = board.GetLength(1);
 
-            for (int i = 0; i < kol; i++)
+            Console.WriteLine();
+
+            if (row - 1 >= 0)
             {
-                for (int j = 0; j < red; j++)
+                if (board[row - 1, col] == '*')
                 {
-                    if (pole[i, j] != '*')
-                    {
-                        char kolkoo = kolko(pole, i, j);
-                        pole[i, j] = kolkoo;
-                    }
-                }
-            }
-        }
-
-        private static char kolko(char[,] r, int rr, int rrr)
-        {
-            int brojkata = 0;
-            int reds = r.GetLength(0);
-            int kols = r.GetLength(1);
-
-            if (rr - 1 >= 0)
-            {
-                if (r[rr - 1, rrr] == '*')
-                {
-                    brojkata++;
+                    piece++;
                 }
             }
 
-            if (rr + 1 < reds)
+            if (row + 1 < reds)
             {
-                if (r[rr + 1, rrr] == '*')
+                if (board[row + 1, col] == '*')
                 {
-                    brojkata++;
+                    piece++;
                 }
             }
 
-            if (rrr - 1 >= 0)
+            if (col - 1 >= 0)
             {
-                if (r[rr, rrr - 1] == '*')
+                if (board[row, col - 1] == '*')
                 {
-                    brojkata++;
+                    piece++;
                 }
             }
 
-            if (rrr + 1 < kols)
+            if (col + 1 < kols)
             {
-                if (r[rr, rrr + 1] == '*')
+                if (board[row, col + 1] == '*')
                 {
-                    brojkata++;
+                    piece++;
                 }
             }
 
-            if ((rr - 1 >= 0) && (rrr - 1 >= 0))
+            if ((row - 1 >= 0) && (col - 1 >= 0))
             {
-                if (r[rr - 1, rrr - 1] == '*')
+                if (board[row - 1, col - 1] == '*')
                 {
-                    brojkata++;
+                    piece++;
                 }
             }
 
-            if ((rr - 1 >= 0) && (rrr + 1 < kols))
+            if ((row - 1 >= 0) && (col + 1 < kols))
             {
-                if (r[rr - 1, rrr + 1] == '*')
+                if (board[row - 1, col + 1] == '*')
                 {
-                    brojkata++;
+                    piece++;
                 }
             }
 
-            if ((rr + 1 < reds) && (rrr - 1 >= 0))
+            if ((row + 1 < reds) && (col - 1 >= 0))
             {
-                if (r[rr + 1, rrr - 1] == '*')
+                if (board[row + 1, col - 1] == '*')
                 {
-                    brojkata++;
+                    piece++;
                 }
             }
 
-            if ((rr + 1 < reds) && (rrr + 1 < kols))
+            if ((row + 1 < reds) && (col + 1 < kols))
             {
-                if (r[rr + 1, rrr + 1] == '*')
+                if (board[row + 1, col + 1] == '*')
                 {
-                    brojkata++;
+                    piece++;
                 }
             }
 
-            return char.Parse(brojkata.ToString());
+            return char.Parse(piece.ToString());
         }
     }
 }
