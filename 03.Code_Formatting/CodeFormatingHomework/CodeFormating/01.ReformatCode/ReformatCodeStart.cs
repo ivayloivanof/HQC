39f7ee9550c02f4341c5827a3b0a3806bf49470e
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-11 00:01:03 +0300

    add new solution and add project 01.ReformatCode for problem 1 from homework assignment

---
+++ 03.Code_Formatting/CodeFormatingHomework/CodeFormating/01.ReformatCode/ReformatCodeStart.cs
@@ -0,0 +1,15 @@
+﻿namespace ReformatCode
+{
+    /// <summary>
+    /// The reformat code.
+    /// </summary>
+    public class ReformatCodeStart
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        public static void Main()
+        {
+        }
+    }
+}
