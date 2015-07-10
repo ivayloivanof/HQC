1c6f4951ad18752e7b5b0d2605876b34954f5b31
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-11 00:11:09 +0300

    add class messages and refactoring

---
+++ 03.Code_Formatting/CodeFormatingHomework/CodeFormating/01.ReformatCode/Messages.cs
@@ -0,0 +1,30 @@
+﻿namespace _01.ReformatCode
+{
+    public static class Messages
+    {
+        public static void EventAdded()
+        {
+            output.Append("Event added\n");
+        }
+
+        public static void EventDeleted(int x)
+        {
+            if (x == 0) NoEventsFound();
+
+            else output.AppendFormat("{0} events deleted\n", x);
+        }
+
+        public static void NoEventsFound()
+        {
+            output.Append("No events found\n");
+        }
+
+        public static void PrintEvent(Event eventToPrint)
+        {
+            if (eventToPrint != null)
+            {
+                output.Append(eventToPrint + "\n");
+            }
+        }
+    }
+}
