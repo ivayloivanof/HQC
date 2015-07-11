525dbf8cc2917925d1e5152d659b739b7db3639a
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-11 21:17:10 +0300

    create MultiDictionary class and inheritnace IDictionary

---
+++ 03.Code_Formatting/CodeFormating/01.ReformatCode/MultiDictionary.cs
@@ -0,0 +1,73 @@
+﻿namespace _01.ReformatCode
+{
+    using System;
+    using System.Collections;
+
+    public class MultiDictionary : IDictionary
+    {
+        public MultiDictionary(bool b)
+        {
+        }
+
+        public bool Contains(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(object key, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionaryEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object this[object key]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public ICollection Keys { get; private set; }
+
+        public ICollection Values { get; private set; }
+
+        public bool IsReadOnly { get; private set; }
+
+        public bool IsFixedSize { get; private set; }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count { get; private set; }
+
+        public object SyncRoot { get; private set; }
+
+        public bool IsSynchronized { get; private set; }
+    }
+}
