b234b1353a0ab8db9122f6eb7d1da56765674780
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-11 21:39:02 +0300

    add new changes SquareRoot file with changes from Program

---
+++ 03.Code_Formatting/CodeFormating/02.ReformatYourOwnCode/SquareRoot.cs
@@ -0,0 +1,38 @@
+﻿namespace _02.ReformatYourOwnCode
+{
+    using System;
+
+    public class SquareRoot
+    {
+        public static void Start()
+        {
+            try
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                SqareRoot(number);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+
+        private static void SqareRoot(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException("number", "Number is negative!");
+            }
+
+            Console.WriteLine(Math.Sqrt(number));
+        }
+    }
+}
