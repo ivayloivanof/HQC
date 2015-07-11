d551afc490633129a168558b19f3f2ee6a0d4d84
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-11 21:17:51 +0300

    create class OrderedBag and inheritance IList

---
+++ 03.Code_Formatting/CodeFormating/01.ReformatCode/OrderedBag.cs
@@ -0,0 +1,75 @@
+﻿namespace _01.ReformatCode
+{
+    using System;
+    using System.Collections;
+
+    public class OrderedBag : IList
+    {
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count { get; private set; }
+
+        public object SyncRoot { get; private set; }
+
+        public bool IsSynchronized { get; private set; }
+
+        public int Add(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsReadOnly { get; private set; }
+
+        public bool IsFixedSize { get; private set; }
+    }
+}
