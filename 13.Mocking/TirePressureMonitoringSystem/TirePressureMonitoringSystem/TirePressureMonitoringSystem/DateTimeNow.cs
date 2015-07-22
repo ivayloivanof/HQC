cb328977f52074390f4c39b1e6e56d2b4439d98e
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-22 21:35:52 +0300

    mock exercise

---
+++ 13.Mocking/TirePressureMonitoringSystem/TirePressureMonitoringSystem/TirePressureMonitoringSystem/DateTimeNow.cs
@@ -0,0 +1,16 @@
+﻿namespace TirePressureMonitoringSystem
+{
+    using System;
+
+    public class DateTimeNow
+    {
+        public static void Main()
+        {
+            DateTime now = DateTime.Now;
+            DateTime tommorow = DateTime.Now.AddDays(1);
+
+            Console.WriteLine(now);
+            Console.WriteLine(tommorow);
+        }
+    }
+}
