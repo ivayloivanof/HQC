96f2093e31f6b50d10f516623eaceec7589f892c
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-22 23:24:34 +0300

    add assertion in defensive programing homework (assertion and exception)

--- 09.DefensiveProgrammingAndException/Assertions-and-Exceptions/Assertions/Assertions.cs
+++ 09.DefensiveProgrammingAndException/Assertions-and-Exceptions/Assertions/Assertions.cs
@@ -1,81 +1,90 @@
-﻿using System;
-
-class Assertions
+﻿namespace Assertions_Homework
 {
-    public static void SelectionSort<T>(T[] arr) where T : IComparable<T>
+    using System;
+    using System.Diagnostics;
+
+    public class Assertions
     {
-        for (int index = 0; index < arr.Length - 1; index++)
+        public static void Main()
         {
-            int minElementIndex = FindMinElementIndex(arr, index, arr.Length - 1);
-            Swap(ref arr[index], ref arr[minElementIndex]);
+            var arr = new int[] { 3, -1, 15, 4, 17, 2, 33, 0 };
+            Console.WriteLine("arr = [{0}]", string.Join(", ", arr));
+            SelectionSort(arr);
+            Console.WriteLine("sorted = [{0}]", string.Join(", ", arr));
+
+            SelectionSort(new int[0]); // Test sorting empty array
+            SelectionSort(new int[1]); // Test sorting single element array
+
+            Console.WriteLine(BinarySearch(arr, -1000));
+            Console.WriteLine(BinarySearch(arr, 0));
+            Console.WriteLine(BinarySearch(arr, 17));
+            Console.WriteLine(BinarySearch(arr, 10));
+            Console.WriteLine(BinarySearch(arr, 1000));
         }
-    }
+
+        public static void SelectionSort<T>(T[] arr) where T : IComparable<T>
+        {
+            Debug.Assert(arr.Length <= 0, "This collection is not correct.");
+
+            for (int index = 0; index < arr.Length - 1; index++)
+            {
+                var minElementIndex = FindMinElementIndex(arr, index, arr.Length - 1);
+                Swap(ref arr[index], ref arr[minElementIndex]);
+            }
+        }
   
-    private static int FindMinElementIndex<T>(T[] arr, int startIndex, int endIndex) 
-        where T : IComparable<T>
-    {
-        int minElementIndex = startIndex;
-        for (int i = startIndex + 1; i <= endIndex; i++)
+        private static int FindMinElementIndex<T>(T[] arr, int startIndex, int endIndex) 
+            where T : IComparable<T>
         {
-            if (arr[i].CompareTo(arr[minElementIndex]) < 0)
+            var minElementIndex = startIndex;
+            for (int i = startIndex + 1; i <= endIndex; i++)
             {
-                minElementIndex = i;
+                if (arr[i].CompareTo(arr[minElementIndex]) < 0)
+                {
+                    minElementIndex = i;
+                }
             }
-        }
-        return minElementIndex;
-    }
 
-    public static int BinarySearch<T>(T[] arr, T value) where T : IComparable<T>
-    {
-        return BinarySearch(arr, value, 0, arr.Length - 1);
-    }
-
-    private static int BinarySearch<T>(T[] arr, T value, int startIndex, int endIndex) 
-        where T : IComparable<T>
-    {
-        while (startIndex <= endIndex)
-        {
-            int midIndex = (startIndex + endIndex) / 2;
-            if (arr[midIndex].Equals(value))
-            {
-                return midIndex;
-            }
-            if (arr[midIndex].CompareTo(value) < 0)
-            {
-                // Search on the right half
-                startIndex = midIndex + 1;
-            }
-            else 
-            {
-                // Search on the right half
-                endIndex = midIndex - 1;
-            }
+            return minElementIndex;
         }
 
-        // Searched value not found
-        return -1;
-    }
+        private static int BinarySearch<T>(T[] arr, T value) where T : IComparable<T>
+        {
+            return BinarySearch(arr, value, 0, arr.Length - 1);
+        }
 
-    static void Main()
-    {
-        int[] arr = new int[] { 3, -1, 15, 4, 17, 2, 33, 0 };
-        Console.WriteLine("arr = [{0}]", string.Join(", ", arr));
-        SelectionSort(arr);
-        Console.WriteLine("sorted = [{0}]", string.Join(", ", arr));
+        private static int BinarySearch<T>(T[] arr, T value, int startIndex, int endIndex) 
+            where T : IComparable<T>
+        {
+            while (startIndex <= endIndex)
+            {
+                var midIndex = (startIndex + endIndex) / 2;
+                if (arr[midIndex].Equals(value))
+                {
+                    return midIndex;
+                }
 
-        SelectionSort(new int[0]); // Test sorting empty array
-        SelectionSort(new int[1]); // Test sorting single element array
+                if (arr[midIndex].CompareTo(value) < 0)
+                {
+                    // Search on the right half
+                    startIndex = midIndex + 1;
+                }
+                else 
+                {
+                    // Search on the right half
+                    endIndex = midIndex - 1;
+                }
+            }
 
-        Console.WriteLine(BinarySearch(arr, -1000));
-        Console.WriteLine(BinarySearch(arr, 0));
-        Console.WriteLine(BinarySearch(arr, 17));
-        Console.WriteLine(BinarySearch(arr, 10));
-        Console.WriteLine(BinarySearch(arr, 1000));
-    }
-    private static void Swap<T>(ref T x, ref T y)
-    {
-        T oldX = x;
-        x = y;
-        y = oldX;
+            // Searched value not found
+            return -1;
+        }
+        
+        private static void Swap<T>(ref T x, ref T y)
+        {
+            var oldX = x;
+            x = y;
+            y = oldX;
+        }
     }
 }
