8c51766704973569f3bd7296500acb1227a2e9ef
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-22 21:48:04 +0300

    add testDrivenDevelopment in project mocking - exercise

---
+++ 13.Mocking/TirePressureMonitoringSystem/TirePressureMonitoringSystem/TestDrivenDevelopmentExercise/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿namespace TestDrivenDevelopmentExercise
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestTreeAdd()
+        {
+        }
+    }
+}
