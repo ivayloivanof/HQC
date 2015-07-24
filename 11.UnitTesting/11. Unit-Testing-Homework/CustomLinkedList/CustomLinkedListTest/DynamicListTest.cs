73ce790eca7fbdf9011e86c3b8f6b6a26a02a600
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-24 22:22:36 +0300

    add homework, create test project CustomLinkedListTest and create first test method TestDynamicListConstructorSetCountToZero

---
+++ 11.UnitTesting/11. Unit-Testing-Homework/CustomLinkedList/CustomLinkedListTest/DynamicListTest.cs
@@ -0,0 +1,31 @@
+﻿namespace CustomLinkedListTest
+{
+    using CustomLinkedList;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DynamicListTest
+    {
+        [TestMethod]
+        public void TestDynamicListConstructorSetCountToZero()
+        {
+            DynamicList<int> dynamicList = new DynamicList<int>();
+            Assert.AreEqual(0, dynamicList.Count, "Dynamic list, constructor not set Count to zero.");
+        }
+
+        [TestMethod]
+        public void TestDynamicListAdd()
+        {
+            DynamicList<int> dynamicList = new DynamicList<int>();
+
+            // Act
+            dynamicList.Add(0);
+            dynamicList.Add(1);
+            dynamicList.Add(2);
+
+            // Assert
+            Assert.AreEqual(0, dynamicList.Count, "Dynamic list, constructor not set Count to zero.");
+        }
+    }
+}
