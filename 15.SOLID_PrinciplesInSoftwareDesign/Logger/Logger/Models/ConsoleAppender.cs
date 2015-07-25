3268d27a0ec34468a9e590ca7541eef77ca9f984
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-25 23:39:49 +0300

    create constructor and property SimpleLayout

--- 15.SOLID_PrinciplesInSoftwareDesign/Logger/Logger/Models/ConsoleAppender.cs
+++ 15.SOLID_PrinciplesInSoftwareDesign/Logger/Logger/Models/ConsoleAppender.cs
@@ -1,6 +1,14 @@
 ﻿namespace Logger.Models
 {
-    public class ConsoleAppender
+    using global::Logger.Interfaces;
+
+    public class ConsoleAppender : IAppender
     {
+        public ConsoleAppender(SimpleLayout simpleLayout)
+        {
+            this.SimpleLayout = simpleLayout;
+        }
+
+        public SimpleLayout SimpleLayout { get; set; }
     }
 }
