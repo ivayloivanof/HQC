06c4ed93cb9cc8f62f1f27c7f485029021d17652
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-25 23:49:22 +0300

    remove property

--- 15.SOLID_PrinciplesInSoftwareDesign/Logger/Logger/Models/SimpleLayout.cs
+++ 15.SOLID_PrinciplesInSoftwareDesign/Logger/Logger/Models/SimpleLayout.cs
@@ -6,8 +6,6 @@
 
     public class SimpleLayout : ILayout
     {
-        private ReportLevel LayoutType { get; set; }
-
         public void Layout(ReportLevel reportLevel, string message)
         {
             var messages = string.Format("<{0}> - <{1}> - <{2}>", DateTime.Now, reportLevel.ToString(), message);
