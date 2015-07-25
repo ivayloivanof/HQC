35bdb453ff00bb49af820b31e29803938fcce2e8
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-25 23:48:50 +0300

    create tests for Logger project

---
+++ 15.SOLID_PrinciplesInSoftwareDesign/Logger/LoggerTest/TestFileAppender.cs
@@ -0,0 +1,16 @@
+﻿namespace LoggerTest
+{
+    using Logger.Models;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TestFileAppender
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            FileAppender fileAppender = new FileAppender(simpleLayout);
+        }
+    }
+}
---
+++ 15.SOLID_PrinciplesInSoftwareDesign/Logger/LoggerTest/TestSimpleLayout.cs
@@ -0,0 +1,16 @@
+﻿namespace LoggerTest
+{
+    using Logger.Models;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TestSimpleLayout
+    {
+        [TestMethod]
+        public void TestSimpleLayoutConstructor()
+        {
+            var simpleLayout = new SimpleLayout();
+        }
+    }
+}
