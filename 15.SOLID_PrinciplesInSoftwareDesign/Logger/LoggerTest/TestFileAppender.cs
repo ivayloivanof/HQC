76c9338afc04d770074d084f6b52af7f7cda96f8
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-26 00:21:02 +0300

    complete first test

--- 15.SOLID_PrinciplesInSoftwareDesign/Logger/LoggerTest/TestFileAppender.cs
+++ 15.SOLID_PrinciplesInSoftwareDesign/Logger/LoggerTest/TestFileAppender.cs
@@ -1,15 +1,9 @@
 ﻿namespace LoggerTest
 {
-    using Logger.Models;
-
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
     public class TestFileAppender
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
     }
 }
--- 15.SOLID_PrinciplesInSoftwareDesign/Logger/LoggerTest/TestSimpleLayout.cs
+++ 15.SOLID_PrinciplesInSoftwareDesign/Logger/LoggerTest/TestSimpleLayout.cs
@@ -1,5 +1,6 @@
 ﻿namespace LoggerTest
 {
+    using Logger.Interfaces;
     using Logger.Models;
 
     using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -8,9 +9,12 @@
     public class TestSimpleLayout
     {
         [TestMethod]
-        public void TestSimpleLayoutConstructor()
+        public void TestSimpleLayoutIsInstanceOfType()
         {
-            var simpleLayout = new SimpleLayout();
+            ILayout simpleLayout = new SimpleLayout();
+            //var messages = simpleLayout.Layout(ReportLevel.Info, "This is test messages.");
+
+            Assert.IsInstanceOfType(simpleLayout, typeof(ILayout), "This SimpleLayout is not type ILayout.");
         }
     }
 }
