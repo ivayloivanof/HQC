50fa95be595f376334edd42862767fc4937cd036
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-26 00:21:57 +0300

    create methodt for logger reports

--- 15.SOLID_PrinciplesInSoftwareDesign/Logger/Logger/Models/Logger.cs
+++ 15.SOLID_PrinciplesInSoftwareDesign/Logger/Logger/Models/Logger.cs
@@ -4,6 +4,31 @@
 
     public class Logger : ILogger
     {
-        private ReportLevel LayoutType { get; set; }
+        private ReportLevel ReportLevel { get; set; }
+
+        public void Info(string message)
+        {
+            this.ReportLevel = ReportLevel.Info;
+        }
+
+        public void Error(string message)
+        {
+            this.ReportLevel = ReportLevel.Error;
+        }
+
+        public void Warning(string message)
+        {
+            this.ReportLevel = ReportLevel.Warning;
+        }
+
+        public void Critical(string message)
+        {
+            this.ReportLevel = ReportLevel.Critical;
+        }
+
+        public void Fatal(string message)
+        {
+            this.ReportLevel = ReportLevel.Fatal;
+        }
     }
 }
