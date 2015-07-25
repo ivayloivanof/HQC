f731e5e707757a12db03aea3cae30bbbc1ef5c24
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-25 23:50:04 +0300

    add property and create simple check

--- 15.SOLID_PrinciplesInSoftwareDesign/Logger/Logger/Models/FileAppender.cs
+++ 15.SOLID_PrinciplesInSoftwareDesign/Logger/Logger/Models/FileAppender.cs
@@ -1,14 +1,37 @@
 ﻿namespace Logger.Models
 {
-    using global::Logger.Interfaces;
+    using System;
+
+    using Interfaces;
 
     public class FileAppender : IAppender
     {
+        private string file;
+
         public FileAppender(SimpleLayout simpleLayout)
         {
             this.SimpleLayout = simpleLayout;
         }
 
         public SimpleLayout SimpleLayout { get; set; }
+
+        public string File
+        {
+            get
+            {
+                return this.file;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("This name of file is empty.");
+                }
+
+                this.file = value;
+            }
+        }
+        
     }
 }
