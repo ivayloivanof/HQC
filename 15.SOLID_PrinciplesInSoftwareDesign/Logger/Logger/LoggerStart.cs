13b94daa5b93b8b27d8d8a4eaa5d96942d647679
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-25 23:40:02 +0300

    update project Logger

--- 15.SOLID_PrinciplesInSoftwareDesign/Logger/Logger/LoggerStart.cs
+++ 15.SOLID_PrinciplesInSoftwareDesign/Logger/Logger/LoggerStart.cs
@@ -7,8 +7,8 @@
         public static void Main()
         {
             var simpleLayout = new SimpleLayout();
-            var consoleAppender = new ConsoleAppender();
-            var fileAppender = new FileAppender();
+            var consoleAppender = new ConsoleAppender(simpleLayout);
+            var fileAppender = new FileAppender(simpleLayout);
 
         }
     }
