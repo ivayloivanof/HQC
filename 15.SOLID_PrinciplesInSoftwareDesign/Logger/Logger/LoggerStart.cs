b521c7d460c0188f70726af720b8ce9d32d84edf
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-26 18:09:34 +0300

    add all source in try - catch block

--- 15.SOLID_PrinciplesInSoftwareDesign/Logger/Logger/LoggerStart.cs
+++ 15.SOLID_PrinciplesInSoftwareDesign/Logger/Logger/LoggerStart.cs
@@ -1,5 +1,7 @@
 ﻿namespace Logger
 {
+    using System;
+
     using Logger.Interfaces;
     using Logger.Models;
 
@@ -7,10 +9,19 @@
     {
         public static void Main()
         {
-            var simpleLayout = new SimpleLayout();
-            IAppender consoleAppender = new ConsoleAppender(simpleLayout);
-            IAppender fileAppender = new FileAppender(simpleLayout);
-            ILogger logger = new Logger(consoleAppender);
+            try
+            {
+                var simpleLayout = new SimpleLayout();
+                IAppender consoleAppender = new ConsoleAppender(simpleLayout);
+                IAppender fileAppender = new FileAppender(simpleLayout);
+                ILogger logger = new Logger(consoleAppender);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            Console.WriteLine();
         }
     }
 }
