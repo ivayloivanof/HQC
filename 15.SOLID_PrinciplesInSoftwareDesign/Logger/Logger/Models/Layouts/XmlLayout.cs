e89cd8321ffac5009d592722a1911d875c2285e7
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-27 00:00:21 +0300

    xmlLayout is complete

--- 15.SOLID_PrinciplesInSoftwareDesign/Logger/Logger/Models/Layouts/XmlLayout.cs
+++ 15.SOLID_PrinciplesInSoftwareDesign/Logger/Logger/Models/Layouts/XmlLayout.cs
@@ -13,17 +13,17 @@
         {
             this.sb.AppendLine("<log>");
 
-            this.sb.AppendLine("\t<date>");
-            this.sb.AppendLine(string.Format("{0}", DateTime.Now));
-            this.sb.AppendLine("\t</date>");
+            this.sb.Append("\t<date>");
+            this.sb.Append(string.Format("{0}", DateTime.Now));
+            this.sb.AppendLine("</date>");
 
-            this.sb.AppendLine("\t<level>");
-            this.sb.AppendLine(string.Format("{0}", reportLevel.ToString()));
-            this.sb.AppendLine("\t</level>");
+            this.sb.Append("\t<level>");
+            this.sb.Append(string.Format("{0}", reportLevel.ToString()));
+            this.sb.AppendLine("</level>");
 
-            this.sb.AppendLine("\t<message>");
-            this.sb.AppendLine(string.Format("{0}", message));
-            this.sb.AppendLine("\t</message>");
+            this.sb.Append("\t<message>");
+            this.sb.Append(string.Format("{0}", message));
+            this.sb.AppendLine("</message>");
 
             this.sb.AppendLine("</log>");
 
