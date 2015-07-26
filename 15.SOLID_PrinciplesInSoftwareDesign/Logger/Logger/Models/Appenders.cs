5d4741086906a07cb7531a0a050f4c1b239e483e
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-26 22:01:25 +0300

    add abstract class Appenders

---
+++ 15.SOLID_PrinciplesInSoftwareDesign/Logger/Logger/Models/Appenders.cs
@@ -0,0 +1,13 @@
+﻿namespace Logger.Models
+{
+    using Interfaces;
+
+    public abstract class Appenders : IAppender
+    {
+        public SimpleLayout SimpleLayout { get; set; }
+
+        public virtual string File { get; set; }
+
+        public abstract void Append(ReportLevel reportLevel, string message);
+    }
+}
