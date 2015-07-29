99260c3a7d427736561c243ed687a2884d750c58
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-29 23:08:41 +0300

    add comment

--- 19.LabTheatre/Huy-Phuong/Huy-Phuong/Exception/TimeDurationOverlapException.cs
+++ 19.LabTheatre/Huy-Phuong/Huy-Phuong/Exception/TimeDurationOverlapException.cs
@@ -2,8 +2,15 @@
 {
     using System;
 
+    /// <summary>
+    /// TimeDurationOverlapException
+    /// </summary>
     public class TimeDurationOverlapException : Exception
     {
+        /// <summary>
+        /// TimeDurationOverlapException constructor
+        /// </summary>
+        /// <param name="mesasge"></param>
         public TimeDurationOverlapException(string mesasge)
             : base(mesasge)
         {
