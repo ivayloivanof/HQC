5f9d21cac6b7e43f2c85c55c54cd48ec2a71b1a9
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-29 18:28:51 +0300

    create class timeDurationOverlapException

---
+++ 19.LabTheatre/Huy-Phuong/Huy-Phuong/Exception/TimeDurationOverlapException.cs
@@ -0,0 +1,12 @@
+﻿namespace Theater.Exception
+{
+    using System;
+
+    public class TimeDurationOverlapException : Exception
+    {
+        public TimeDurationOverlapException(string mesasge)
+            : base(mesasge)
+        {
+        }
+    }
+}
