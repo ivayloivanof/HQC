93b5f978ad62173a66f138a711892d9f899922db
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-29 18:21:23 +0300

    refactoring matrica (matrix) project homework

---
+++ 17.Refactoring/Matrica/ChangeMatrix.cs
@@ -0,0 +1,77 @@
+﻿namespace WalkInMatrix
+{
+    public static class ChangeMatrix
+    {
+        private static int[] dirX = { 1, 1, 1, 0, -1, -1, -1, 0 };
+        private static int[] dirY = { 1, 0, -1, -1, -1, 0, 1, 1 };
+
+        public static void ChangeFieldOfMatrix(ref int dx, ref int dy)
+        { 
+            var cd = 0;
+
+            for (var count = 0; count < 8; count++)
+            {
+                if (dirX[count] == dx && dirY[count] == dy)
+                {
+                    cd = count;
+                    break;
+                }
+            }
+
+            if (cd == 7)
+            {
+                dx = dirX[0]; 
+                dy = dirY[0]; 
+                return;
+            }
+
+            dx = dirX[cd + 1];
+            dy = dirY[cd + 1];
+        }
+
+        public static bool CheckingMatrix(int[,] arr, int x, int y)
+        {
+            for (var i = 0; i < 8; i++)
+            {
+                if (x + dirX[i] >= arr.GetLength(0) || x + dirX[i] < 0)
+                {
+                    dirX[i] = 0;
+                }
+
+                if (y + dirY[i] >= arr.GetLength(0) || y + dirY[i] < 0)
+                {
+                    dirY[i] = 0;
+                }
+            }
+
+            for (var i = 0; i < 8; i++)
+            {
+                if (arr[x + dirX[i], y + dirY[i]] == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static void FindCell(int[,] arr, out int x, out int y)
+        {
+            x = 0;
+            y = 0;
+
+            for (var row = 0; row < arr.GetLength(0); row++)
+            {
+                for (var col = 0; col < arr.GetLength(0); col++)
+                {
+                    if (arr[row, col] == 0)
+                    {
+                        x = row; 
+                        y = col; 
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 17.Refactoring/Matrica/WalkInMatrixStart.cs
@@ -0,0 +1,78 @@
+﻿namespace WalkInMatrix
+{
+    using System;
+
+    public class WalkInMatrixStart
+    {
+        public static void Main()
+        {
+            var step = 3;
+            int[,] matrix = new int[step, step];
+            int k = 1, i = 0, j = 0, dx = 1, dy = 1;
+
+            while (true)
+            { 
+                matrix[i, j] = k;
+                if (!ChangeMatrix.CheckingMatrix(matrix, i, j))
+                {
+                    break;
+                }
+
+                while (i + dx >= step || i + dx < 0 || j + dy >= step || j + dy < 0 || matrix[i + dx, j + dy] != 0)
+                {
+                    ChangeMatrix.ChangeFieldOfMatrix(ref dx, ref dy);
+                }
+
+                i += dx; 
+                j += dy; 
+                k++;
+            }
+
+            for (var row = 0; row < step; row++)
+            {
+                for (var col = 0; col < step; col++)
+                {
+                    Console.Write("{0,3}", matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+
+            ChangeMatrix.FindCell(matrix, out i, out j);
+
+            if (i != 0 && j != 0)
+            {
+                dx = 1; 
+                dy = 1;
+
+                while (true)
+                {
+                    matrix[i, j] = k;
+                    if (!ChangeMatrix.CheckingMatrix(matrix, i, j))
+                    {
+                        break;
+                    }
+
+                    while (i + dx >= step || i + dx < 0 || j + dy >= step || j + dy < 0 || matrix[i + dx, j + dy] != 0)
+                    {
+                        ChangeMatrix.ChangeFieldOfMatrix(ref dx, ref dy);
+                    }
+
+                    i += dx; 
+                    j += dy; 
+                    k++;
+                }
+            }
+
+            for (var row = 0; row < step; row++)
+            {
+                for (var col = 0; col < step; col++)
+                {
+                    Console.Write("{0,3}", matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
